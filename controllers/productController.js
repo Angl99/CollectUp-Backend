06b3bef1b76cb72bbf6aa5a5841ab75c286f85c8
GyancarlosPinto <gyancarlospinto@pursuit.org>
2024-07-18 00:09:06 -0400

    Implemented a new prisma database helper to simplify product controller code.

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,5 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+const { prisma } = require('../helpers/prismaDbHelper')
+
 
 const productController = {
   // Get all products
