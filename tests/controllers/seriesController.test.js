a44113b6d0bd0a90cae15dca68b1fc980a07b310
GyancarlosPinto <gyancarlospinto@pursuit.org>
2024-07-17 23:40:24 -0400

    Added a helper function to access the Prisma client.

--- tests/controllers/seriesController.test.js
+++ tests/controllers/seriesController.test.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
 const app = require('../../index');
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+const { prisma } = require('../../helpers/prismaDbHelper')
+
 
 describe('Series Controller', () => {
   let createdSeriesId;
